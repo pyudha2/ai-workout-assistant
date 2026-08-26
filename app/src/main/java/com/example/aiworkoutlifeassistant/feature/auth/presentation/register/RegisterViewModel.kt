38fe4a2190022ddf27381161e7d7ac135f51251a
Rikuga <pranatayudhapratama20@gmail.com>
2026-08-26 22:40:06 +0700

    feat: continue setup firebase auth (register)

--- app/src/main/java/com/example/aiworkoutlifeassistant/feature/auth/presentation/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/aiworkoutlifeassistant/feature/auth/presentation/register/RegisterViewModel.kt
@@ -11,3 +11,27 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor(
+    private val authRepository: AuthRepository
+) : ViewModel() {
+    private val _registerState = MutableStateFlow<Resource<User>?>(null)
+    val registerState: StateFlow<Resource<User>?> = _registerState.asStateFlow()
+
+    fun register(name: String, email: String, password: String, confirmPassword: String){
+        if (name.isBlank() || email.isBlank() || password.isBlank()){
+            _registerState.value = Resource.Error("Semua Kolom Wajib Diisi")
+            return
+        }
+        if(password != confirmPassword){
+            _registerState.value = Resource.Error("Password Tidak Sama")
+            return
+        }
+        viewModelScope.launch {
+            authRepository.register(name, email, password).collect {
+                result -> _registerState.value = result
+            }
+        }
+    }
+}
